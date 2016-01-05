f0353f5a323a3c453c31a437a7f1b90dd0f6952f
Murilo Bastos <contato@murilobast.com>
2016-01-05 03:41:25 -0200

    [0.6.0] Chest rework 1/1 style and collection schema

---
+++ collections/chest.js
@@ -0,0 +1,42 @@
+Chest = new Mongo.Collection('chest');
+
+Chest.attachSchema(new SimpleSchema({
+	id: {
+		type: Number,
+		label: "ID",
+		defaultValue: 0
+	},
+	name: {
+		type: String,
+		label: "Name",
+		defaultValue: 0
+	},
+	items: {
+		type: Array,
+		label: 'Items',
+	},
+	'items.$': {
+		type: Object
+	},
+	'items.$.name': {
+		type: String,
+		label: "Name",
+		defaultValue: 'frenesi'
+	},
+	'items.$.weight': {
+		type: Number,
+		label: "weight",
+		decimal: true,
+		defaultValue: 0
+	},
+	'items.$.amount': {
+		type: Number,
+		label: "weight",
+		defaultValue: 0
+	},
+	'items.$.id': {
+		type: Number,
+		label: "weight",
+		defaultValue: 0
+	}
+}));
