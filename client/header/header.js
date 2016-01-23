aaee5de5a3a21d5fa6a7743615659c91425ca3e0
Murilo Bastos <contato@murilobast.com>
2016-01-23 21:24:37 -0200

    [0.9.1] Pushing assets and format number lib

---
+++ client/header/header.js
@@ -0,0 +1,24 @@
+Template.header.events({
+	'click #menu': function (e, t) {
+		var $sideBar = $('#side');
+		$sideBar.toggleClass('isOpen');
+	},
+
+	'click .header__content__links': function (e, t) {
+		var target = e.target;
+		if (target.id === 'mask' || target.tagName === 'A') {
+			$('#side').removeClass('isOpen');
+		}
+	}
+});
+
+Template.header.helpers({
+	isActive: function (name) {
+		if (Router.current()) {
+			var curLocation = Router.current().route.getName();
+			if (curLocation === name) {
+				return 'active';
+			}
+		}
+	}
+});
