85ab53c02de24c0f6fb113d8aa2ae9a4e683719c
Murilo Bastos <contato@murilobast.com>
2016-03-15 00:06:00 -0300

    [1.0.8] Home and basic changelog

--- client/home/home.js
+++ client/home/home.js
@@ -1,6 +1,7 @@
 Template.home.helpers({
 	changelogs: function () {
 		let changelogs = Changelog.find({}, {sort: {timestamp: 'desc'}}) || false;
+		
 		if (changelogs.count()) {
 			return changelogs;
 		}
