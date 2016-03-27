6fd387e36403f5cfebe428771e8e22b1cf39335d
Murilo Bastos <contato@murilobast.com>
2016-03-27 16:33:49 -0300

    [1.3.1] Setting metags for SEO

--- SEO.js
+++ SEO.js
@@ -1,28 +1,28 @@
 Meteor.startup(function () {
- if (Meteor.isClient) {
-			return SEO.config({
-				title: 'PW Simulator',
-				meta: {
-					'description': 'PW Simulator é uma ferramente para simular Drops de Báus e Reforja de Equipamentos em Perfect World'
-				},
-				meta: {
-					'google-site-verification': 'xK1TMl_QbLUOrMU0oqXYNZZrNjUBpLgF8Nl07Qh1QV4'
-				},
-				og: {
-					'site_name': 'PW Simulator' 
-				},
-				og: {
-					'title': 'PW Simulator' 
-				},
-				og: {
-					'image': 'launcher-icon-0-75x.png' 
-				},
-				og: {
-					'type': 'website' 
-				},
-				og: {
-					'locale': 'pt_br' 
-				}
-			});
-		}
+	if (Meteor.isClient) {
+		return SEO.config({
+			title: 'PW Simulator',
+			meta: {
+				'description': 'PW Simulator é uma ferramente para simular Drops de Báus e Reforja de Equipamentos em Perfect World'
+			},
+			meta: {
+				'google-site-verification': 'xK1TMl_QbLUOrMU0oqXYNZZrNjUBpLgF8Nl07Qh1QV4'
+			},
+			og: {
+				'site_name': 'PW Simulator' 
+			},
+			og: {
+				'title': 'PW Simulator' 
+			},
+			og: {
+				'image': 'launcher-icon-0-75x.png' 
+			},
+			og: {
+				'type': 'website' 
+			},
+			og: {
+				'locale': 'pt_br' 
+			}
+		});
+	}
 });
