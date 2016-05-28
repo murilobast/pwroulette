f05c1fa50d3fd204cd6690192fed9f308a3d691a
Murilo Bastos <contato@murilobast.com>
2016-05-28 13:25:18 -0300

    [2.3.3] Updating prerender.io package

--- server/prerenderio.js
+++ server/prerenderio.js
@@ -1,3 +1,3 @@
-prerenderio.set('prerenderServiceUrl', 'http://pwsimulator.com/');
+prerenderio.set('serviceUrl', 'http://service.prerender.io/');
 prerenderio.set('prerenderToken', 'cWRG3ggVx7Asq8R20Yoh');
 WebApp.connectHandlers.use(prerenderio);
