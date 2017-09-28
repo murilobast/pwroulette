ab306387a5090527b969b95c31986defa5be8aec
Murilo Silva <msilva@redventures.com>
2017-09-28 17:27:38 -0300

    Removing trivia urls from offiline url list

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,22 +6,28 @@ const routes = [
 	'/trivia'
 ]
 
-const slugify = string => string.toLowerCase()
-	.replace(/ /g, '-')
-	.replace(/\%/, '')
-	.replace(/\//, '')
-	.replace(/\\/, '')
+// const slugify = string => string.toLowerCase()
+// 	.replace(/ /g, '-')
+// 	.replace(/\%/, '')
+// 	.replace(/\//, '')
+// 	.replace(/\\/, '')
 
 const getTriviaRoutes = () => {
-	const parentRoutes = triviaData.map(({ slug, questions }) => {
-		const url = `/trivia/${slug}`
-		return [...questions
-			.map(({ question }, i) => `${url}/${i}-${slugify(question)}`), url]
-	})
-	const triviaRoutes = parentRoutes
-		.reduce((prev, next) => [...prev, ...next], [])
-	return [...routes, ...triviaRoutes]
+	const parentRoutes = triviaData
+		.map(({ slug, questions }) => `/trivia/${slug}`)
+	return [...routes, ...parentRoutes]
 }
 
+// To heavy
+// const getTriviaRoutes = () => {
+// 	const parentRoutes = triviaData.map(({ slug, questions }) => {
+// 		const url = `/trivia/${slug}`
+// 		return [...questions
+// 			.map(({ question }, i) => `${url}/${i}-${slugify(question)}`), url]
+// 	})
+// 	const triviaRoutes = parentRoutes
+// 		.reduce((prev, next) => [...prev, ...next], [])
+// 	return [...routes, ...triviaRoutes]
+// }
 
 module.exports = getTriviaRoutes()
