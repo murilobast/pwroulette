14a0268ca6fa57c9265e928679ddde88b1936fdf
Murilo Silva <msilva@redventures.com>
2017-09-29 20:28:02 -0300

    Improving metatags

--- src/helpers/generateMetatags.js
+++ src/helpers/generateMetatags.js
@@ -3,6 +3,7 @@ const generateMetatags = ({ title, tag, description, color = '#1b2126', url }) =
 	<link key="canonical" rel="canonical" href={`http://pwsimulator.com/${url}`} />,
 	<meta key="og:title" property="og:title" content={`${title} | PW Simulator`} />,
 	<meta key="og:url" property="og:url" content={`http://pwsimulator.com/${url}`} />,
+	<meta key="og:type" property="og:type" content="website" />,
 	<meta key="og:description" property="og:description" content={description} />,
 	<meta key="description" name="description" content={description} />,
 	<meta key="theme-color" name="theme-color" content={color} />
